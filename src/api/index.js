1e4f0563cd2c4c03eb4330f2a70b86bc13ad75a1
Vyacheslav <viacheslav.saiapin@gmail.com>
2023-02-10 17:40:07 +0300

    fix api

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,9 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 const BASE_URL = process.env.REACT_APP_BASEURL;
 
 const getData = createAsyncThunk("getData", async (path, { rejectWithValue }) => {
+  const route = path === "products" ? "products" : "additives"
   try {
-    const response = await fetch(`${BASE_URL}/${path}`);
+    const response = await fetch(`${BASE_URL}/${route}?category=`+path);
     if (response.status !== 200) {
       throw new Error(response.statusText);
     }
