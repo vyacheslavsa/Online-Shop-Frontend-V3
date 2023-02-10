b7cfcab8573a4490daddf6b4681349e1ea540d65
Vyacheslav <viacheslav.saiapin@gmail.com>
2023-02-10 17:46:31 +0300

    fix api

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const BASE_URL = process.env.REACT_APP_BASEURL;
 const getData = createAsyncThunk("getData", async (path, { rejectWithValue }) => {
   const route = path === "products" ? "products" : "additives"
   try {
-    const response = await fetch(`${BASE_URL}/${route}?category=`+path);
+    const response = await fetch(`${BASE_URL}/${route}?category=${path}`);
     if (response.status !== 200) {
       throw new Error(response.statusText);
     }
