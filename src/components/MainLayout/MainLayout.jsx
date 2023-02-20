31e83291cfbbf4957903b5815b9efcf8bf44b4db
Vyacheslav <viacheslav.saiapin@gmail.com>
2023-02-20 12:39:12 +0300

    check privat route

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -5,8 +5,17 @@ import SideBar from '../SideBar/SideBar'
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
 import './MainLayout.css'
 import {TEXT_HEADER} from "../../constans";
+import Button from '../Button/Button';
+import $apiAuth from '../../http/auth'
 
 const MainLayout = ({children}) => {
+
+
+    const createOrder = async () => {
+        const result = await $apiAuth.get('/createOrder')
+        console.log('message', result.data.message)
+    }
+
     return (
         <>
             <Header text={TEXT_HEADER}/>
@@ -14,6 +23,7 @@ const MainLayout = ({children}) => {
                 <div className="left_content">
                     <SideBar/>
                     <ShoppingCart/>
+                    <Button onClick={createOrder}>Приват Роут</Button>
                 </div>
                 {children}
             </div>
